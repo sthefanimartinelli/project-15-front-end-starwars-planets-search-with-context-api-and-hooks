954023d83567dbbd213e405b33c54709e23ccdd9
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2023-02-27 17:19:39 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
+import PlanetsContext from './context/PlanetsContext';
+import Table from './components/Table';
+// import Data from './components/Data';
 import './App.css';
 
 function App() {
+  const [planets, setPlanets] = useState([]);
+
+  const context = useMemo(() => ({
+    planets,
+    setPlanets,
+  }), [planets, setPlanets]);
+
+  useEffect(() => {
+    fetch('https://swapi.dev/api/planets')
+      .then((result) => result.json())
+      .then((data) => setPlanets(data.results))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
-    <span>Hello, App!!</span>
+    <PlanetsContext.Provider value={ context }>
+      <Table />
+    </PlanetsContext.Provider>
   );
 }
 
